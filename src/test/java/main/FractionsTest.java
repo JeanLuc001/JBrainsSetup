37aa482d74d07a1ffa76207a9ac364aba05c45b6
JeanLuc001 <sebastian.kramer@web.de>
2018-08-09 18:49:40 +0200

    ref: adding assertEqualFraction() for fraction comparision

--- src/test/java/main/FractionsTest.java
+++ src/test/java/main/FractionsTest.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 
 public class FractionsTest
 {
+	private void assertEqualFraction(Fraction f, int expectedNumerator, int expectedDenominator)
+	{
+		assertTrue("wrong numerator", f.numerator() == expectedNumerator);
+		assertTrue("wrong denominator", f.denominator() == expectedDenominator);
+	}
+	
 	@Test
 	public void when_adding_0_over_1_to_0_over_1_expect_0_over_1()
 	{
 		Fraction f1 = Fraction.of(0, 1);
 		Fraction f2 = Fraction.of(0, 1);
 		Fraction added = f1.add(f2);
-		assertTrue("wrong numerator", added.numerator() == 0);
-		assertTrue("wrong denominator", added.denominator() == 1);
+		assertEqualFraction(added, 0, 1);
 	}
 
 	@Test
@@ -22,8 +27,7 @@ public class FractionsTest
 		Fraction f1 = Fraction.of(7, 3);
 		Fraction f2 = Fraction.of(4, 5);
 		Fraction added = f1.add(f2);
-		assertTrue("wrong numerator", added.numerator() == 47);
-		assertTrue("wrong denominator", added.denominator() == 15);
+		assertEqualFraction(added, 47,15);
 	}
 	// @Test
 	// public void when_adding_5_over_5_to_7_over_3_then_expect_10_over_3()
